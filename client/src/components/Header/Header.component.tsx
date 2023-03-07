8d2e391ec550f3de437223a044b11a50d8927368
nguyynavu <nguyenanhvu.work@gmail.com>
2023-03-07 09:03:33 +0700

    Init code

--- client/src/components/Header/Header.component.tsx
+++ client/src/components/Header/Header.component.tsx
@@ -1,12 +1,16 @@
 import Link from "next/link";
 import React from "react";
 import routes from "utils/routes";
+import Image from "next/image";
+import Logo from "@public/images/logo.png";
 
 const Header = () => {
   return (
     <div className="py-5">
       <div>
-        <Link href={routes.home}></Link>
+        <Link href={routes.home}>
+          <Image src={Logo} alt="logo" />
+        </Link>
       </div>
     </div>
   );
